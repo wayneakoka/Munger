ebb005ab5e3ba73d96fb77446cc2c31bd4a5f591
wayneakoka <jedechire>
2016-11-29 10:42:37 -0500

    added QR code reader

--- app/src/main/java/com/example/panda/munger/toolFragment.java
+++ app/src/main/java/com/example/panda/munger/toolFragment.java
@@ -1,13 +1,18 @@
 package com.example.panda.munger;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.widget.Button;
-import android.widget.TextView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Bundle;
-
+import android.widget.Button;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.widget.Toast;
 
 
 /**
@@ -16,34 +21,103 @@ import android.os.Bundle;
 
 public class toolFragment extends Fragment {
 
+    Button scanner;
+    Button scanner2;
+
+    static final String ACTION_SCAN = "com.google.zxing.client.android.SCAN";
+    Intent intent = new Intent(ACTION_SCAN);
+
     public static toolFragment newInstance() {
         toolFragment fragment = new toolFragment();
         return fragment;
     }
 
-    public toolFragment() {
-    }
-
-    Button ClickMe;
-    TextView tv;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.toolfragment, container, false);
-        ClickMe = (Button) rootView.findViewById(R.id.button);
-        tv = (TextView) rootView.findViewById(R.id.textView2);
+        scanner = (Button) rootView.findViewById(R.id.scanner);
+        scanner2 = (Button) rootView.findViewById(R.id.scanner2);
 
-        ClickMe.setOnClickListener(new View.OnClickListener() {
+        scanner.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                if(tv.getText().toString().contains("Hello")){
-                    tv.setText("Hi");
-                }else tv.setText("Hello");
+            public void onClick(View v) {
+                scanQR(v);
+            }
+        });
+        scanner2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                scanBar(v);
             }
         });
         return rootView;
     }
 
 
+            //product barcode mode
+    public void scanBar(View v) {
+        try {
+            //start the scanning activity from the com.google.zxing.client.android.SCAN intent
+            Intent intent = new Intent(ACTION_SCAN);
+            intent.putExtra("SCAN_MODE", "PRODUCT_MODE");
+            startActivityForResult(intent, 0);
+        } catch (ActivityNotFoundException anfe) {
+            //on catch, show the download dialog
+            showDialog(getActivity(), "No Scanner Found", "Download a scanner code activity?", "Yes", "No").show();
+        }
+    }
+
+    //product qr code mode
+    public void scanQR(View v) {
+        try {
+            //start the scanning activity from the com.google.zxing.client.android.SCAN intent
+            Intent intent = new Intent(ACTION_SCAN);
+            intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
+            startActivityForResult(intent, 0);
+        } catch (ActivityNotFoundException anfe) {
+            //on catch, show the download dialog
+            showDialog(getActivity(), "No Scanner Found", "Download a scanner code activity?", "Yes", "No").show();
+        }
+    }
+
+    //alert dialog for downloadDialog
+    private static AlertDialog showDialog(final Activity act, CharSequence title, CharSequence message, CharSequence buttonYes, CharSequence buttonNo) {
+        AlertDialog.Builder downloadDialog = new AlertDialog.Builder(act);
+        downloadDialog.setTitle(title);
+        downloadDialog.setMessage(message);
+        downloadDialog.setPositiveButton(buttonYes, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Uri uri = Uri.parse("market://search?q=pname:" + "com.google.zxing.client.android");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                try {
+                    act.startActivity(intent);
+                } catch (ActivityNotFoundException anfe) {
+
+                }
+            }
+        });
+        downloadDialog.setNegativeButton(buttonNo, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialogInterface, int i) {
+            }
+        });
+        return downloadDialog.show();
+    }
+
+    //on ActivityResult method
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (requestCode == 0) {
+            if (resultCode == Activity.RESULT_OK) {
+                //get the extras that are returned from the intent
+                String contents = intent.getStringExtra("SCAN_RESULT");
+                String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
+                Toast toast = Toast.makeText(getActivity(), "Content:" + contents + " Format:" + format, Toast.LENGTH_LONG);
+                toast.show();
+            }
+        }
+    }
+
+
+
 }
+
