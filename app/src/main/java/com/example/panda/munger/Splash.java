b4c4a4a2df63de1c0cb0e0d60a5f476c845d97b9
wayneakoka <jedechire>
2016-12-03 18:08:52 -0500

    added splash added facebook login

---
+++ app/src/main/java/com/example/panda/munger/Splash.java
@@ -0,0 +1,30 @@
+package com.example.panda.munger;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class Splash extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Boolean isFirstRun = getSharedPreferences("PREFERENCE", MODE_PRIVATE).getBoolean("isfirstrun", true);
+
+        if (isFirstRun){
+            Toast.makeText(Splash.this, "first run", Toast.LENGTH_LONG).show();
+            getSharedPreferences("PREFERENCE", MODE_PRIVATE).edit().putBoolean("isfirstrun", false).commit();
+            Intent intent = new Intent(Splash.this, FacebookLogin.class);
+            startActivity(intent);
+            finish();
+        }else{
+            Intent intent = new Intent(Splash.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
+
+    }
+}
